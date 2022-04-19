201494f80bdbd4f289272685d66b311d9547ab86
Kpasula <kpasula@unomaha.edu>
2022-04-19 17:00:29 -0500

    Added Conversations

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Conversations\SelectSomethingConversation;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 use App\Conversations\ExampleConversation;
-use BotMan\Drivers\Telegram\TelegramDriver;
 
 class BotManController extends Controller
 {
@@ -14,22 +14,10 @@ class BotManController extends Controller
      */
     public function handle()
     {
-        //info('Incoming call', \request()->all());
-
         $botman = app('botman');
 
-        try {
-            $botman->listen();
-        } catch (\Exception $e) {
-            info('error Incoming call', \request()->all());
-            info('error catched: '.$e->getMessage());
-            $fromId = request()->all()['message']['from']['id'] ?? request()->all()['callback_query']['from']['id'];
+        $botman->listen();
 
-            $botman->say('🚧 Something did not go as planned. 😕 We are sorry.', $fromId, TelegramDriver::class);
-            $botman->say('Please try to /start the game again or contact Christoph on Twitter https://twitter.com/christophrumpel.',
-                $fromId, TelegramDriver::class);
-
-        }
     }
 
     /**
@@ -42,11 +30,10 @@ class BotManController extends Controller
 
     /**
      * Loaded through routes/botman.php
-     *
-     * @param BotMan $bot
+     * @param  BotMan $bot
      */
     public function startConversation(BotMan $bot)
     {
-        $bot->startConversation(new ExampleConversation());
+        $bot->startConversation(new SelectSomethingConversation());
     }
 }
