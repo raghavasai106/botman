68d7d2c483dabf4d879bf38e52ffe6dcfd101df4
Raghavasai Kowlasker <rkowlasker@unomaha.edu>
2022-04-29 13:50:14 -0500

    Updated with Dialogflow

--- routes/botman.php
+++ routes/botman.php
@@ -9,3 +9,22 @@ $botman->hears('Hello', function($bot) {
 $botman->hears('Appointment', function ($bot){
     $bot->startConversation(new \App\Http\Conversations\OnboardingConversation());
 });
+
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
+$dialogflow = \BotMan\BotMan\Middleware\Dialogflow::create('')->listenForAction();
+$dialogflow = \BotMan\Middleware\DialogFlow\V2\DialogFlow::create('en');
+$botman->middleware->received($dialogflow);
+$botman->hears('(.*)', function ($bot) {
+    $extras = $bot->getMessage()->getExtras();
+    $bot->reply($extras['apiReply']);
+})->middleware($dialogflow);
+
+//$botman->hears('intent-action-name', function (BotMan\BotMan $bot) {
+//    // The incoming message matched the "my_api_action" on Dialogflow
+//    // Retrieve Dialogflow information:
+//    $extras = $bot->getMessage()->getExtras();
+//
+//    $apiReply = $extras['apiReply'];
+//    $apiAction = $extras['apiAction'];
+//    $apiIntent = $extras['apiIntent'];
+//})->middleware($dialogflow);
